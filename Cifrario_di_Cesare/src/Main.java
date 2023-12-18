2fd2845d1f19ff32f3e9b10c0652973bc0aada4f
ERIN.MUSAJ <ERIN.MUSAJ@buonarroti.tn.it>
2023-12-18 09:24:59 +0100

    finished

---
+++ Cifrario_di_Cesare/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static String cifrario(String s){
+        String rit = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int ascii = (int)c;
+            if((65<=ascii && ascii>=90) || (97<=ascii && ascii>=122)){
+                c = (char)(ascii==122||ascii==90? ascii-25:ascii+1);
+            }
+            rit+=c+"";
+        }
+        return rit;
+    }
+
+    public static String decifrario(String s){
+        String rit = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int ascii = (int)c;
+            if((65<=ascii && ascii<=90) || (97<=ascii && ascii<=122)){
+                c = (char)(ascii==97||ascii==65? ascii+25:ascii-1);
+            }
+            rit+=c+"";
+        }
+        return rit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cifrario("abczZ ."));
+        System.out.println(decifrario(cifrario("abczZ .")));
+    }
+}
